dc4ac85dda29a1b0f69004f148ff2032c37daec2
Holleyfeline <98298986+Holleyfeline@users.noreply.github.com>
2022-01-23 20:16:57 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Sad)
+    soundExpression.sad.playUntilDone()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Happy)
+    soundExpression.giggle.playUntilDone()
+})
+basic.showIcon(IconNames.Asleep)
